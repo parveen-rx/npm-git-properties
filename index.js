c2098218eda5799895c265ad42ec69cec8d150bc
Parveen Soni <parveen.soni@rxlogix.com>
2024-02-02 23:30:51 +0530

    Minor code refactoring

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const gracefulFsLib = require('graceful-fs');
 const pathLib = require('path'); /*useful to detect path separator for a platform*/
 const shellLib = require('shelljs');
 
-const _isNativeExecSync = childProcessLib.hasOwnProperty('spawnSync');
-const _pathSeparator = pathLib.sep;
-const _refBranch = /^ref: refs\/heads\/(.*)\n/;
-const _currentRepoName = 'NPM-GIT-PROPERTIES';
-const _detachedAtHeadPrefix = 'Detached At Head: ';
+const hasNativeExecSync = childProcessLib.hasOwnProperty('spawnSync');
+const pathSeparator = pathLib.sep;
+const refBranch = /^ref: refs\/heads\/(.*)\n/;
+const currentRepoName = 'NPM-GIT-PROPERTIES';
+const detachedAtHeadPrefix = 'Detached At Head: ';
 
 const _exeCmd = (cmd, args) => {
     let result;
-    if (_isNativeExecSync) {
+    if (hasNativeExecSync) {
         result = childProcessLib.spawnSync(cmd, args);
     } else {
         result = shellLib.exec(`${cmd} ${args.join(' ')}`, { silent: true });
     }
     if (result.status !== 0) {
         const errorDetails = result.stderr || result.stdout;
-        const errorMessage = `${_currentRepoName} has failed to execute command: ${errorDetails}`;
+        const errorMessage = `${currentRepoName} has failed to execute command: ${errorDetails}`;
         throw new Error(errorMessage);
     }
     return result.stdout.toString('utf8').trim();
@@ -32,9 +32,9 @@ const _getGitDir = (dirPath) => {
         dirPath = module.parent.filename;
     }
     if (typeof dirPath === 'string') {
-        dirPath = dirPath.split(_pathSeparator);
+        dirPath = dirPath.split(pathSeparator);
     }
-    const gitRepoPath = dirPath.join(_pathSeparator);
+    const gitRepoPath = dirPath.join(pathSeparator);
     let testPath = gitRepoPath;
     if (!testPath.length) {
         //if current repo is not a git directory, then throw error
@@ -50,34 +50,34 @@ const _getGitDir = (dirPath) => {
             if (gracefulFsLib.existsSync(parentRepoPath)) {
                 return pathLib.resolve(parentRepoPath);
             }
-            throw new Error(_currentRepoName + ' could not find repository from path ' + parentRepoPath);
+            throw new Error(currentRepoName + ' could not find repository from path ' + parentRepoPath);
         }
         return testPath;
     }
     //try with parent path unless found or throw an error
     dirPath.pop();
     return _getGitDir(dirPath);
-}
+};
 
 const currentBranch = (dir) => {
     const gitDir = _getGitDir(dir);
     const head = gracefulFsLib.readFileSync(pathLib.resolve(gitDir, 'HEAD'), 'utf8');
-    const b = head.match(_refBranch);
+    const b = head.match(refBranch);
     if (b) {
         return b[1];
     } else {
-        return _detachedAtHeadPrefix + head.trim();
+        return detachedAtHeadPrefix + head.trim();
     }
-}
+};
 
 const shortHash = (dir, size) => {
     return fullHash(dir).substr(0, size || 7);
-}
+};
 
 const fullHash = (dir) => {
     const currBranch = currentBranch(dir);
-    if (currBranch.startsWith(_detachedAtHeadPrefix)) {
-        return currBranch.substr(_detachedAtHeadPrefix.length);
+    if (currBranch.startsWith(detachedAtHeadPrefix)) {
+        return currBranch.substr(detachedAtHeadPrefix.length);
     }
     const gitDir = _getGitDir(dir);
     const gitRootDir = gitDir.indexOf('.git/worktrees/') > 0 ?
@@ -94,35 +94,35 @@ const fullHash = (dir) => {
         ref = pkgFileRegex.exec(pkgFileContents)[1];
     }
     return ref.trim();
-}
+};
 
 const lastGitMsg = () => {
   return _exeCmd('git', ['log', '-1', '--pretty=%B']);
-}
+};
 
 const gitTag = (flagDirty) => {
     if (flagDirty) {
         return _exeCmd('git', ['describe', '--always', '--tag', '--dirty', '--abbrev=0']);
     }
     return _exeCmd('git', ['describe', '--always', '--tag', '--abbrev=0']);
-}
+};
 
 const countOfAllCommits = () => {
     return parseInt(_exeCmd('git', ['rev-list', '--all', '--count']), 10);
-}
+};
 
 const dateOfLastCommit = () => {
     return new Date(_exeCmd('git', ['log', '--no-color', '-n', '1', '--pretty=format:"%ad"']));
-}
+};
 
 const hasUnStagedChanges = () => {
     const writeTree = _exeCmd('git', ['write-tree']);
     return _exeCmd('git', ['diff-index', writeTree, '--']).length > 0;
-}
+};
 
 const isDirty = () => {
     return _exeCmd('git', ['diff-index', 'HEAD', '--']).length > 0;
-}
+};
 
 const isTagDirty = () => {
     try {
@@ -134,18 +134,18 @@ const isTagDirty = () => {
         throw e;
     }
     return false;
-}
+};
 
 const remoteUrl = () => {
     return _exeCmd('git', ['ls-remote', '--get-url']);
-}
+};
 
 const tagFirstParent = (flagDirty) => {
     if (flagDirty) {
         return _exeCmd('git', ['describe', '--always', '--tag', '--dirty', '--abbrev=0', '--first-parent']);
     }
     return _exeCmd('git', ['describe', '--always', '--tag', '--abbrev=0', '--first-parent']);
-}
+};
 
 const fullGitInfoAsJson = (appParams) => {
     const buildDetails = {
@@ -207,7 +207,7 @@ const fullGitInfoAsJson = (appParams) => {
     } else {
         return {"git": fullGitInfo};
     }
-}
+};
 
 module.exports = {
     currentBranch : currentBranch,
