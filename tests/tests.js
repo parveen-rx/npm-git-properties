ea99bc866176d17bcafe3bc233766fda2dfc6ef1
Parveen Soni <parveen.soni@rxlogix.com>
2024-02-05 22:39:14 +0530

    Fix test cases

--- tests/tests.js
+++ tests/tests.js
@@ -1,43 +1,76 @@
 const assert = require('assert');
 const git = require('../index');
 
+const customPropMap = new Map();
+const userName = "App User";
+const userEmail = "appuser@app.com";
+
+customPropMap.set("git.build.user.name", userName);
+customPropMap.set("git.build.user.email", userEmail);
+
 let result;
 
-result = git.shortHash();
-assert.equal(result.length > 3, true, 'short() returns string of length 4+');
-
-result = git.shortHash(null, 8);
-assert.equal(result.length === 8, true, 'short() returns string of length 4+');
-
-result = git.fullHash();
-assert.equal(result.length > 38, true, 'long() returns string of length 39+');
-
+// Test currentBranch
 result = git.currentBranch();
 assert.equal(!!result.length, true, 'currentBranch() returns a string with non-zero length');
 
+// Test buildHost
+result = git.buildHost();
+assert.equal(!!result.length, true, 'buildHost() returns a string with non-zero length');
+
+// Test buildVersion
+result = git.buildVersion();
+assert.equal(!!result.length, true, 'buildVersion() returns a string with non-zero length');
+
+// Test commitIdAbbrev
+result = git.commitIdAbbrev();
+assert.equal(result.length >= 7, true, 'commitIdAbbrev() returns string of length 7 chars');
+
+// Test commitIdFull
+result = git.commitIdFull();
+assert.equal(result.length === 40, true, 'commitIdFull() returns string of length 40 chars');
+
+// Test lastCommitMsg
+result = git.lastCommitMsg();
+assert.equal(!!result.length, true, 'lastCommitMsg() returns a string with non-zero length');
+
+// Test commitUserInfo
+result = git.commitUserInfo();
+assert.equal(!!result.length, true, 'commitUserInfo() returns a string with non-zero length');
+
+// Test dateOfLastCommit
+result = git.dateOfLastCommit();
+assert.equal(result.constructor === String && !!result.length, true, 'dateOfLastCommit() returns a date');
+
+// Test isDirty
+result = git.isDirty();
+assert.equal(result.constructor === Boolean, true, 'isDirty() returns a boolean value');
+
+// Test remoteUrl
+result = git.remoteUrl();
+assert.equal(result.indexOf("https://github.com") > -1 || result.indexOf("git@github.com") > -1, true, "remoteUrl() returns expected remote URL value");
+
+// Test commitIdDescAndTags
+result = git.commitIdDescAndTags();
+assert.equal(!!result.length, true, 'commitIdDescAndTags(flagDirty: true) returns a string with non-zero length');
+
+// Test closestTagCommitCount
+result = git.closestTagCommitCount();
+assert.equal(result.constructor === String && !!result.length, true, 'closestTagCommitCount() returns an error description');
+
+// Test countOfAllCommits
 result = git.countOfAllCommits();
 assert.notEqual(result, 0, 'countOfAllCommits() returns a non-zero number');
 assert.equal(Math.abs(result), result, 'countOfAllCommits() returns a positive number');
 
-result = git.dateOfLastCommit();
-assert.equal(result instanceof Date, true, 'dateOfLastCommit() returns a date');
+// Test gitInfoAsJson
+result = git.gitInfoAsJson(new Map(), true);
+assert.equal(result.constructor === Object, true, 'gitInfoAsJson returns an object');
 
-result = git.isDirty();
-assert.equal(typeof result, 'boolean', 'isDirty() returns a boolean');
+result = git.gitInfoAsJson();
+assert.equal(result.constructor === String && JSON.parse(result).constructor === Object, true, 'gitInfoAsJson returns a stringify JSON Object');
 
-result = git.isTagDirty();
-assert.equal(typeof result, 'boolean', 'isTagDirty() returns a boolean');
+result = git.gitInfoAsJson(customPropMap, true);
+assert.equal(result["git"]["build"]["user"]["name"] === userName && result["git"]["build"]["user"]["email"] === userEmail, true, 'gitInfoAsJson overrides result with custom property map');
 
-result = git.lastGitMsg();
-assert.equal(!!result.length, true, 'lastGitMsg() returns a string with non-zero length');
-
-result = git.commitIdDescAndTags();
-assert.equal(!!result.length, true, 'commitIdDescAndTags() returns a string with non-zero length');
-
-result = git.tagFirstParent();
-assert.equal(result.length !== 0, true, 'tagFirstParent() returns a string with non-zero length');
-
-result = git.remoteUrl();
-assert.equal(result.indexOf("https://github.com") === 0 || result.indexOf("git@github.com") === 0, true, "remoteUrl() returns unexpected value: '" + result + "'");
-
-console.log('All test cases passed');
+console.log('All test cases passed...');
